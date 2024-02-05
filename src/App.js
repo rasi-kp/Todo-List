126e0ae43164e485700abbe702a66e6cde90e25f
Rasi K P <rasir239@gmail.com>
2024-02-05 22:17:00 +0530

    clear some error

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [toDos,setTodos]=useState([])
-  const [todo,settodo]=useState('')
-  const handleDelete=(id)=>{
-    setTodos(toDos.filter((todo)=>todo.id!=id))
+  const [toDos, setTodos] = useState([])
+  const [todo, settodo] = useState('')
+  const handleDelete = (id) => {
+    setTodos(toDos.filter((todo) => todo.id != id))
+  }
+  const onclicked=()=>{
+      if (todo.trim() !== '') {
+          setTodos([...toDos, { id: Date.now(), text: todo, status: false }]);
+          settodo('');
+      }
   }
   return (
     <div className="app">
@@ -15,38 +21,46 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Hi ,How are you 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input value={todo} onChange={(e)=>settodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setTodos([...toDos,{id:Date.now(), text:todo,status:false}])} className="fas fa-plus"></i>
+        <input value={todo} onChange={(e) => settodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={onclicked} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-      { toDos.map((value)=>{
+        {toDos.map((value, index) => {
 
-      return ( <div className="todo">
-          <div className="left">
-            <input onChange={(e)=>{
-              console.log(e.target.checked)
-              setTodos(toDos.filter(val=>{
-                if(val.id===value.id){
-                  val.status=e.target.checked
-                }
-                return val
-              }))
-            }} value={value.status} type="checkbox" name="" id="" />
-            <p>{value.text}</p>
-          </div>
-          <div className="right">
-            <i onClick={() => handleDelete(value.id)} className="fas fa-times"></i>
-          </div>
-        </div> )
-      }) }
-      {toDos.map((val)=>{
-        if(val.status){
-          return(<h1>{val.text}</h1>)
+          return (
+            <>
+              <div className="todo">
+                <div className="left">
+
+                  <input onChange={(e) => {
+                    console.log(e.target.checked)
+                    setTodos(toDos.filter(val => {
+                      if (val.id === value.id) {
+                        val.status = e.target.checked
+                      }
+                      return val
+                    }))
+                  }} value={value.status} type="checkbox" name="" id="" />
+                  <h4 style={{ marginLeft: "10px" }}>{index + 1}.</h4>
+                  <p> {value.text} </p>
+                </div>
+                <div className="right">
+                  <i onClick={() => handleDelete(value.id)} className="fas fa-times"></i>
+                </div>
+              </div>
+            </>
+          )
+        })}
+        {
+          toDos.map((val) => {
+            if (val.status) {
+              return (<h1>{val.text}</h1>)
+            }
+          })
         }
-      })}
       </div>
     </div>
   );
